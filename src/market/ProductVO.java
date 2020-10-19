244853743a23da999098c1a232368c1b3ef2bac5
lyj6054 <lyj6054@naver.com>
2020-10-19 14:57:07 +0900

    add ProductVO

---
+++ src/market/ProductVO.java
@@ -0,0 +1,46 @@
+package market;
+
+public class ProductVO {
+	//Field
+	String pid, pname, address, explain, sysdate;
+	int pirce;
+	public String getPid() {
+		return pid;
+	}
+	public void setPid(String pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getExplain() {
+		return explain;
+	}
+	public void setExplain(String explain) {
+		this.explain = explain;
+	}
+	public String getSysdate() {
+		return sysdate;
+	}
+	public void setSysdate(String sysdate) {
+		this.sysdate = sysdate;
+	}
+	public int getPirce() {
+		return pirce;
+	}
+	public void setPirce(int pirce) {
+		this.pirce = pirce;
+	}
+	
+	
+
+}
