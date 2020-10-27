aa19d1ed8326926554a09b25d4cae244f772cb83
yhse0924 <dudghk0924@naver.com>
2020-10-27 11:32:02 +0900

    finish

--- src/market/MarketMgmJoin.java
+++ src/market/MarketMgmJoin.java
@@ -50,11 +50,8 @@ public class MarketMgmJoin {
 		joinOff();
         main.showPane.setVisible(false);
        
-<<<<<<< HEAD
         joinBackPane = new ImageJoinPanel(new ImageIcon("C:\\dev\\se_workspace\\market\\images\\join_back.png" ).getImage());
-=======
-        joinBackPane = new ImageJoinPanel(new ImageIcon("C:\\dev\\eclipse_workspace\\market\\images\\join_back.png").getImage());
->>>>>>> master
+//        joinBackPane = new ImageJoinPanel(new ImageIcon("C:\\dev\\eclipse_workspace\\market\\images\\join_back.png").getImage());
 
 // ¿µÀç-"C:/java_workspace/market/images/join_back.png").getImage());
 //¹Î¼® - C:\\dev\\se_workspace\\sist_project_1\\images\\join_back.png").getImage();
