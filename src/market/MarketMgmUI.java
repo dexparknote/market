a811b6d3cb7ca4545e3cda73d1ce3bbcb82a754a
Dex_Park <mns0327@naver.com>
2020-10-28 14:08:06 +0900

    c

--- src/market/MarketMgmUI.java
+++ src/market/MarketMgmUI.java
@@ -81,7 +81,7 @@ public class MarketMgmUI extends JFrame {
 		public void showMain() {  //10.22 영재 수정
 			
 
-			showPane = new ImagePanel(new ImageIcon("C:/java_workspace/market/images/login_main.png").getImage());
+			showPane = new ImagePanel(new ImageIcon("C:/dev/se_workspace/sist_project_1/images/login_main.png").getImage());
 
 
 			//영재-C:/java_workspace/market/images/login_main.png
