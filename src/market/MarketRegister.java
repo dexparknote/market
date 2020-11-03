e4b92246df5539e4edce182b21ace0013ae7bd6e
yhse0924 <dudghk0924@naver.com>
2020-11-03 10:25:09 +0900

    a

--- src/market/MarketRegister.java
+++ src/market/MarketRegister.java
@@ -230,7 +230,8 @@ public class MarketRegister {
 		//서버와 연결이 되있지 않다면 서버와 연결
 		if(mvo.getServer_state()==0) {
 			main.serverConnect(); //server_state vo에서 int로 바꿔야함 ㅠ
-			main.now_room=main.system.get_pid(Integer.parseInt(main.vo.getId()));
+			int id = Integer.parseInt(main.vo.getId());
+			main.now_room=main.system.get_pid(id);
 			System.out.println(main.now_room);
 		}
 		
