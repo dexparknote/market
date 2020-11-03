bbf55167a7b35c34d3c6fd9395723871886a0c70
yhse0924 <dudghk0924@naver.com>
2020-11-03 13:03:49 +0900

    pull

--- src/market/MarketMgmJoin.java
+++ src/market/MarketMgmJoin.java
@@ -47,16 +47,14 @@ public class MarketMgmJoin {
 	 */
 	public void join() {
 		joinOff();
-//        main.showPane.setVisible(false);
-
-
+        main.showPane.setVisible(false);
 
 //        joinBackPane = new ImageJoinPanel(new ImageIcon("C:/java_workspace/market/images/join_back.png").getImage());
-//        joinBackPane = new ImageJoinPanel(new ImageIcon("C:\\dev\\se_workspace\\market\\images\\join_back.png").getImage()); //¿µÈ­
+        joinBackPane = new ImageJoinPanel(new ImageIcon("C:\\dev\\se_workspace\\market\\images\\join_back.png").getImage()); //¿µÈ­
 //        joinBackPane = new ImageJoinPanel(new ImageIcon("C:/dev/se_workspace/sist_project_1/images/join_back.png").getImage()); //¹Î¼®
 //		main.showPane.setVisible(false);
 //        joinBackPane = new ImageJoinPanel(new ImageIcon("C:/java_workspace/market/images/join_back.png").getImage());
-        joinBackPane = new ImageJoinPanel(new ImageIcon("C:/dev/se_workspace/sist_project_1/images/join_back.png").getImage()); //¹Î¼®
+//        joinBackPane = new ImageJoinPanel(new ImageIcon("C:/dev/se_workspace/sist_project_1/images/join_back.png").getImage()); //¹Î¼®
 
 
 // ¿µÀç-"C:/java_workspace/market/images/join_back.png").getImage());
